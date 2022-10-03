2e4dc1a0f06c3139a7db9aeddc8daf94d569081d
Ala Williams <alicja.m.williams@gmail.com>
2022-10-03 07:59:40 +0200

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const addButtons = document.querySelectorAll(".btn-add");
-//adding items
-addButtons.forEach((btn) => btn.addEventListener ('click', () => {
-  
-}))
+const addBtn = document.querySelector('.btn-add');
+
+
+const showIncome = (e) => {
+  let nameInput = document.querySelector('.income-text-field');
+  let numberInput = document.querySelector('.income-number-field');
+  let nameVal = nameInput.value;
+  let numberVal = numberInput.value;
+  console.log(nameVal, numberVal);
+  e.preventDefault();
+}
+
+addBtn.addEventListener('click', showIncome);
+
+
+
+
+
+
+
